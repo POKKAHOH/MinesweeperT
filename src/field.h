15374a9a9cce319e6c2ac95cc1a7c38e923c0d03
POKKAHOH <pokkahoha@ya.ru>
2018-11-07 06:57:38 +0300

    Create field.h

---
+++ src/field.h
@@ -0,0 +1,30 @@
+#ifndef FIELD_H
+# define FIELD_H
+
+class Cell;
+
+// игровое поле
+class Field {
+public:
+  Field();
+  ~Field();
+  void Play();
+protected:
+// константы, определяющие игру
+  const static int m_n = 8;        // размер поля
+  const static int m_mineNumber = 10; // количество мин на поле
+  const static int m_free = m_n * m_n - m_mineNumber; // число клеток без мин
+// переменные
+  int m_opened = 0;                // число открытых клеток (проще проверять выигрыш)
+
+  void openAll();                  // открывает всё поле
+  bool isValidCoord(int i, int j); // i,j в поле?
+  void openCell(int i, int j);     // открывает клетку
+  bool isWin();                   // проверяет выигрыш
+
+  Cell* m_field[m_n][m_n];         // игровое поле
+  bool m_isGameActive;             // состояние игрового поля
+
+};
+
+#endif // FIELD_H
