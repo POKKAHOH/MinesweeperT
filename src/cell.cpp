f83b7a32824669dba0d00ba52a3b6a804b7d95d7
POKKAHOH <pokkahoha@ya.ru>
2018-11-08 06:30:31 +0300

    Correct paint
    Careful reading terminfo - way to happy

--- src/cell.cpp
+++ src/cell.cpp
@@ -22,21 +22,26 @@ void Cell::setOpen() { m_isOpen = true; paint(); }
 void Cell::swapFlag() { m_isFlag = !m_isFlag; paint(); }
 
 void Cell::paint() {
-  cout<<"\e["<<(m_i-1)<<";"<<m_j<<"H"<<delim_l;
+//some black magic...
+  int X,Y;
+  X=3+m_j;   //3 line from top to field
+  Y=3*m_i+5; //5 char from left side to field, 3 char to cell
+//walk to ligth side...
+  cout<<"\e["<<X<<";"<<(Y-1)<<"H"<<delim_l;
   if (isOpen()) /*open field*/
     if (isMine()) /*write red star*/
-      cout<<"\e[31m\e["<<m_i<<";"<<m_j<<"H"<<mine_c<<"\e[0m";
+      cout<<"\e[31m\e["<<X<<";"<<Y<<"H"<<mine_c<<"\e[0m";
     else /*opening field*/
       if (value()) /*near mine*/
-        cout<<"\e[3"<<value()<<"m\e["<<m_i<<";"<<m_j<<"H"<<value()<<"\e[0m";
+        cout<<"\e[3"<<value()<<"m\e["<<X<<";"<<Y<<"H"<<value()<<"\e[0m";
       else /*empty field*/
-        cout<<"\e["<<m_i<<";"<<m_j<<"H"<<empty_c;
+        cout<<"\e["<<X<<";"<<Y<<"H"<<empty_c;
   else /*switch flag*/
     if (m_isFlag) /*write blue >*/
-      cout<<"\e[34m\e["<<m_i<<";"<<m_j<<"H"<<flag_c<<"\e[0m";
+      cout<<"\e[34m\e["<<X<<";"<<Y<<"H"<<flag_c<<"\e[0m";
     else  /*write close field*/
-    cout<<"\e["<<m_i<<";"<<m_j<<"H"<<closed_c;
+    cout<<"\e["<<X<<";"<<Y<<"H"<<closed_c;
 
-  cout<<"\e["<<(m_i+1)<<";"<<m_j<<"H"<<delim_r;
+  cout<<"\e["<<X<<";"<<(Y+1)<<"H"<<delim_r;
   return;
 }
